00363b5c086a3b8c230e2a7ef10e5abb33ed7df4
segin-GH <segin.bytes@gmail.com>
2022-06-14 07:59:41 +0530

    led that dim's.

---
+++ DimmableLed/src/main.cpp
@@ -0,0 +1,24 @@
+# include <Arduino.h>
+
+int ledPin = 9;
+int brightness ; //  use value from 0 to 255
+int delayTime = 100;
+
+void setup()
+{
+  pinMode(ledPin,INPUT);
+}
+
+void loop ()
+{
+    for(brightness = 0 ;brightness > 255; brightness++)
+    {
+      analogWrite(ledPin,brightness);
+      delay(delayTime);
+    }
+    for(brightness = 255 ;brightness < 0; brightness --)
+    {
+      analogWrite(ledPin,brightness);
+      delay(delayTime);
+    }
+}
