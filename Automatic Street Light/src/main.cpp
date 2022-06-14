e44952c6ae9e2af72ec8678a42815247bafcd94c
segin-GH <segin.bytes@gmail.com>
2022-06-14 09:14:42 +0530

    .

--- Automatic Street Light/src/main.cpp
+++ Automatic Street Light/src/main.cpp
@@ -1,9 +1,29 @@
-#include <Arduino.h>
+# include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+int ldrPin = A0;
+int relayPin = 2;
+int ldrVal;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ldrPin,INPUT);
+  pinMode(relayPin,OUTPUT);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  ldrVal = analogRead(ldrPin);
+
+  if (ldrVal <= 250)
+  {
+    digitalWrite(relayPin,LOW);
+    Serial.println("Light OFF");
+  }
+  else if (ldrVal >= 1000 )
+  {
+    digitalWrite(relayPin,HIGH);
+    Serial.println("Light ON");
+  }
 }
