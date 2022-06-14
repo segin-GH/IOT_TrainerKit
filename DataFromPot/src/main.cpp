89d919c69483b640c28f3a0625eb2e9df4f59499
segin-GH <segin.bytes@gmail.com>
2022-06-14 08:09:03 +0530

    getting data from pot.

---
+++ DataFromPot/src/main.cpp
@@ -0,0 +1,17 @@
+# include <Arduino.h>
+
+int potPin = A0;
+int potVal;
+
+void setup()
+{
+  pinMode(potPin,INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  potVal = analogRead(potPin);
+  Serial.print(potVal);
+
+}
