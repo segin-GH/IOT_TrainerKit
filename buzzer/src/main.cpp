2ccf791fd5ad1f9b10b03c7debb35064df6f48f3
segin-GH <segin.bytes@gmail.com>
2022-06-17 13:45:49 +0530

    .

---
+++ buzzer/src/main.cpp
@@ -0,0 +1,25 @@
+# include <Arduino.h>
+
+void setup()
+{
+  pinMode(2,OUTPUT);
+
+}
+
+void loop ()
+
+{
+  digitalWrite(2,HIGH);
+  delay(100);
+  digitalWrite(2,LOW);
+  delay(100);
+
+  digitalWrite(2,HIGH);
+  delay(250);
+  digitalWrite(2,LOW);
+  delay(250);
+  
+
+
+
+}
